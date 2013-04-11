c7f84b958fd897af5f95ec165a6c27ed30bbe4e9
bkling <bkling@L11715038604.wctc.msft>
2013-04-11 16:36:35 -0500

    Had some issues with lab 4, I fixed them in class today and put it to Github.

--- src/lab4/InputOutputGui.java
+++ src/lab4/InputOutputGui.java
@@ -4,11 +4,13 @@ import javax.swing.JOptionPane;
 
 /**
  * This class is responsible for all input and output in the program.
- * 
- * @author  Jim Lombardo, jlombardo@wctc.edu
+ *
+ * @author Jim Lombardo, jlombardo@wctc.edu
  * @version 1.00
  */
 public class InputOutputGui {
+
+    String lastName;
     private NameService nameService;
 
     public InputOutputGui() {
@@ -16,12 +18,18 @@ public class InputOutputGui {
     }
 
     public void startConversation() throws InvalidNameException {
-        
+
         String fullName = JOptionPane.showInputDialog("Enter full name:");
-        String lastName = nameService.extractLastName(fullName);
+        try {
+            lastName = nameService.extractLastName(fullName);
+        } catch (InvalidNameException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "Input Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } finally {
+            System.out.println("Output to console.");
+        }
         String msg = "Your last name is: " + lastName;
         JOptionPane.showMessageDialog(null, msg);
-        
+
     }
-     
 }
